5a82394666c35abd1e98c46c7a574502ec71d607
sammycaglar <caglarsemih1@outlook.com>
2024-03-25 17:38:21 +0300

    US 307 Folder Created for Testing

---
+++ src/US_307/TC_0701.java
@@ -0,0 +1,20 @@
+package US_307;
+
+import Utility.BaseDriver;
+import org.testng.annotations.Test;
+
+public class TC_0701 extends BaseDriver {
+
+    @Test
+    public void AccesstoEJunkieHomePagefromShopdemoPage(){
+
+
+
+
+
+
+
+
+    }
+
+}
---
+++ src/US_307/TC_0701_Elements.java
@@ -0,0 +1,30 @@
+package US_307;
+
+import Utility.BaseDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class TC_0701_Elements {
+
+    public TC_0701_Elements() {
+        PageFactory.initElements(BaseDriver.driver, this);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
