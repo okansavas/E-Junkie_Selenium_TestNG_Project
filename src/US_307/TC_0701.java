1ad9bc046afcae1096310cd6d6e5b86ea730fea5
sammycaglar <caglarsemih1@outlook.com>
2024-03-27 01:06:03 +0300

    US_307 automation codes written and test passed.

--- src/US_307/TC_0701.java
+++ src/US_307/TC_0701.java
@@ -1,14 +1,34 @@
 package US_307;
 
 import Utility.BaseDriver;
+import Utility.Tools;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.security.Key;
 
 public class TC_0701 extends BaseDriver {
 
     @Test
-    public void AccesstoEJunkieHomePagefromShopdemoPage(){
+    public void AccesstoEJunkieHomePagefromShopdemoPage() {
 
+        Actions driverAksiyon = new Actions(driver);
+        TC_0701_Elements tce07 = new TC_0701_Elements();
 
+        Tools.JScriptClick(tce07.eJShoplink);
+        String eJunkieUrl = driver.getCurrentUrl();
+        System.out.println("eJunkieUrl = " + eJunkieUrl);
+
+        wait.until(ExpectedConditions.urlContains("e-junkie"));
+
+        if (tce07.getItNow.getText().equals("GET IT NOW")) {
+            driverAksiyon.keyUp(Keys.ESCAPE);
+        }
+
+        Assert.assertTrue(eJunkieUrl.contains("e-junkie"), "E-Junkie site could not open");
 
 
 
--- src/US_307/TC_0701_Elements.java
+++ src/US_307/TC_0701_Elements.java
@@ -1,6 +1,9 @@
 package US_307;
 
 import Utility.BaseDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class TC_0701_Elements {
@@ -9,25 +12,11 @@ public class TC_0701_Elements {
         PageFactory.initElements(BaseDriver.driver, this);
     }
 
+    @FindBy(xpath = "//a[@class='EJ-ShopLink']")
+    public WebElement eJShoplink;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    @FindBy(xpath = "//div[@class='wrapper']/a")
+    public WebElement getItNow;
 
 
 }
